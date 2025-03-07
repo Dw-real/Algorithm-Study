6320e5ee31e5e96e47b1e79c40b3f2a03f898f6b
hahahanDW <97527425+Dw-real@users.noreply.github.com>
2025-03-07 18:16:54 +0900

    [Bronze V] Title: A/B, Time: 212 ms, Memory: 17788 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        double a = scanner.nextInt();
+        double b = scanner.nextInt();
+        System.out.println(a / b);
+        scanner.close();
+    }
+}
