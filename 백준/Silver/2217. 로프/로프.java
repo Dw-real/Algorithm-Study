846d7c0930ce52decc7e5c9d37317b14335bbed5
hahahanDW <97527425+Dw-real@users.noreply.github.com>
2025-03-08 14:33:58 +0900

    [Silver IV] Title: 로프, Time: 336 ms, Memory: 27704 KB -BaekjoonHub

---
+++ 백준/Silver/2217. 로프/로프.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[] rope = new int[n];
+
+        for (int i=0; i<n; i++) {
+            rope[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(rope);
+
+        int count = n;
+        int max = 0;
+        for (int i=0; i<rope.length; i++) {
+            max = Math.max(max, rope[i] * count--);
+        }
+
+        bw.write(max + "\n");
+
+        br.close();
+        bw.close();
+    }
+}
