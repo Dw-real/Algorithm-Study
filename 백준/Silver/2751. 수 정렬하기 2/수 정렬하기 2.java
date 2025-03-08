03e3dc853073f96bce05b80029891fc347461f32
hahahanDW <97527425+Dw-real@users.noreply.github.com>
2025-03-08 14:37:53 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1036 ms, Memory: 112828 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -0,0 +1,74 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int[] tmp;
+
+    static void merge_sort(int[] arr, int left, int right) {
+        if (left >= right) 
+            return;
+
+        int mid = (left + right) / 2;
+        merge_sort(arr, left, mid);
+        merge_sort(arr, mid + 1, right);
+        merge(arr, left, mid, right);
+    }
+
+    static void merge(int[] arr, int left, int mid, int right) {
+        int l = left;
+        int r = mid + 1;
+        int idx = left;
+
+        while (l <= mid && r <= right) {
+            if (arr[l] <= arr[r]) {
+                tmp[idx] = arr[l];
+                idx++;
+                l++;
+            }
+            else {
+                tmp[idx] = arr[r];
+                idx++;
+                r++;
+            }
+        }
+
+        while (l <= mid) {
+            tmp[idx] = arr[l];
+            idx++;
+            l++;
+        }
+
+        while (r <= right) {
+            tmp[idx] = arr[r];
+            idx++;
+            r++;
+        }
+
+        for (int i=left; i<=right; i++) {
+            arr[i] = tmp[i];
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        tmp = new int[n];
+
+        for (int i=0; i<n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        
+        merge_sort(arr, 0, n - 1);
+        
+        for (int i=0; i<n; i++) {
+            bw.write(arr[i] + "" + "\n");
+        }
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
