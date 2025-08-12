592b5b5f9d16ccfde8cb78e3037c06b5fb1506c7
Dw-real <97527425+Dw-real@users.noreply.github.com>
2025-08-12 11:55:04 +0900

    [Silver II] Title: N과 M (9), Time: 256 ms, Memory: 36572 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.java
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -1,52 +1,62 @@
 import java.io.*;
-import java.util.*;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
 
 public class Main {
-    static TreeSet<String> set;
-    static StringBuilder sb;
+    static StringBuilder sb = new StringBuilder();
+    static Set<String> set = new HashSet<>(); // 중복 수열 제거 용도
 
-    static void permutation(int[] arr, boolean[] visited, String output, int n, int m, int depth) {
-        if (depth == m) {
-            output = output.substring(1);
-            if (!set.contains(output)) {
-                sb.append(output).append("\n");
-                set.add(output);
+    static void permutation(int[] arr, int[] output, boolean[] visited,
+                            int n, int r, int depth) {
+
+        if (depth == r) {
+            StringBuilder str = new StringBuilder();
+
+            for (int num : output) {
+                str.append(num).append(" ");
+            }
+
+            if (!set.contains(str.toString())) {
+                set.add(str.toString());
+                sb.append(str);
+                sb.append("\n");
             }
             return;
         }
-        for (int i=0; i<arr.length; i++) {
-                if (!visited[i]) {
-                    visited[i] = true;
-                    permutation(arr, visited, output + " " + arr[i], n, m, depth + 1);
-                    visited[i] = false;
-                }
+
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                output[depth] = arr[i];
+                permutation(arr, output, visited, n, r,depth + 1);
+                visited[i] = false;
+            }
         }
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        
-        set = new TreeSet<String>();
-        sb = new StringBuilder();
+
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
         int[] arr = new int[n];
-
         st = new StringTokenizer(br.readLine(), " ");
-        for (int i=0; i<n; i++) {
+        for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
         Arrays.sort(arr);
 
-        permutation(arr, new boolean[n], "", n, m, 0);
+        permutation(arr, new int[m], new boolean[n], n, m,0);
 
         bw.write(sb.toString());
 
         bw.flush();
-        br.close();
         bw.close();
+        br.close();
     }
 }
