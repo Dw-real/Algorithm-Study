a603910bfd8f0c90e7e776be2b943a520ebc3b05
Dw-real <97527425+Dw-real@users.noreply.github.com>
2025-08-12 13:04:23 +0900

    [Silver II] Title: N과 M (10), Time: 104 ms, Memory: 14300 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.java
@@ -0,0 +1,62 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class Main {
+    static StringBuilder sb = new StringBuilder();
+    static Set<String> set = new HashSet<>(); // 중복 수열 제거 용도
+
+    static void permutation(int[] arr, int[] output, boolean[] visited,
+                            int n, int r, int start, int depth) {
+
+        if (depth == r) {
+            StringBuilder str = new StringBuilder();
+
+            for (int num : output) {
+                str.append(num).append(" ");
+            }
+
+            if (!set.contains(str.toString())) {
+                set.add(str.toString());
+                sb.append(str);
+                sb.append("\n");
+            }
+            return;
+        }
+
+        for (int i = start; i < n; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                output[depth] = arr[i];
+                permutation(arr, output, visited, n, r, i,depth + 1);
+                visited[i] = false;
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n];
+        st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        permutation(arr, new int[m], new boolean[n], n, m,0, 0);
+
+        bw.write(sb.toString());
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
