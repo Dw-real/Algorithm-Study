90340aec17cf6f079bc85869f69b479753482350
Dw-real <97527425+Dw-real@users.noreply.github.com>
2025-08-18 22:22:40 +0900

    [Silver II] Title: 병사 배치하기, Time: 152 ms, Memory: 15348 KB -BaekjoonHub

--- 백준/Silver/18353. 병사 배치하기/병사 배치하기.java
+++ 백준/Silver/18353. 병사 배치하기/병사 배치하기.java
@@ -3,31 +3,43 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
+    static int[] arr;
+    static int[] dp;
+    static int n;
+
+    static int LDS(int n) {
+        if (dp[n] == -1) {
+            dp[n] = 1; // 어느 위치든 가장 긴 감소하는 부분 수열의 길이는 1
+
+            for (int i = n - 1; i >= 0; i--) {
+                if (arr[n] < arr[i]) {
+                    dp[n] = Math.max(dp[n], LDS(i) + 1);
+                }
+            }
+        }
+        return dp[n];
+    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int n = Integer.parseInt(br.readLine()); // 병사 수
-        int[] arr = new int[n];
-        int[] dp = new int[n];
+        n = Integer.parseInt(br.readLine()); // 병사 수
+        arr = new int[n];
+        dp = new int[n];
+
+        Arrays.fill(dp, -1);
 
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        Arrays.fill(dp, 1); // 어느 위치든 가장 긴 감소하는 부분 수열의 길이는 1
+        Arrays.fill(dp, -1); // 어느 위치든 가장 긴 감소하는 부분 수열의 길이는 1
 
-        int max = 0; // 남길 수 있는 최대 병사 수
-
+        int max = 0;
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < i; j++) {
-                if (arr[i] < arr[j]) {
-                    dp[i] = Math.max(dp[i], dp[j] + 1);
-                }
-            }
-            max = Math.max(max, dp[i]);
+            max = Math.max(max, LDS(i));
         }
 
         bw.write(n - max + "\n");
