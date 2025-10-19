c41fc687dab78c974a078d3b59b866dc113052ce
Dw-real <97527425+Dw-real@users.noreply.github.com>
2025-10-19 15:24:13 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 592 ms, Memory: 70344 KB -BaekjoonHub

--- 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.java
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.java
@@ -2,11 +2,11 @@ import java.io.*;
 import java.util.*;
 
 class Node implements Comparable<Node> {
-    int e;
+    int v;
     int w;
 
-    public Node(int e, int w) {
-        this.e = e;
+    public Node(int v, int w) {
+        this.v = v;
         this.w = w;
     }
 
@@ -17,37 +17,35 @@ class Node implements Comparable<Node> {
 }
 
 public class Main {
-    static int[] dist;
-    static ArrayList<Node>[] graph;
+    static PriorityQueue<Node> nodes;
+    static ArrayList<ArrayList<Node>> graph;
     static boolean[] visited;
-    static int INF = 2000001;
-    static int n;
+    static int[] dist;
+    static int n, e;
+    static int INF = 10000000;
 
-    static int dijkstra(int start, int end) {
-        PriorityQueue<Node> pq = new PriorityQueue<>();
+    static void dijkstra(int start) {
+        nodes = new PriorityQueue<>();
         dist = new int[n + 1];
-        Arrays.fill(dist, INF);
         visited = new boolean[n + 1];
-
+        Arrays.fill(dist, INF);
+        nodes.add(new Node(start, 0));
         dist[start] = 0;
-        pq.add(new Node(start, 0));
 
-        while (!pq.isEmpty()) {
-            Node now = pq.poll();
-            int v = now.e;
+        while (!nodes.isEmpty()) {
+            Node now = nodes.poll();
+            int v = now.v;
 
             if (visited[v]) continue;
             visited[v] = true;
 
-            for (Node next : graph[v]) {
-                if (dist[next.e] > dist[v] + next.w) {
-                    dist[next.e] = dist[v] + next.w;
-                    pq.add(new Node(next.e, dist[next.e]));
+            for (Node next : graph.get(v)) {
+                if (dist[next.v] > dist[v] + next.w) {
+                    dist[next.v] = dist[v] + next.w;
+                    nodes.add(new Node(next.v, dist[next.v]));
                 }
             }
         }
-
-        return dist[end];
     }
 
     public static void main(String[] args) throws IOException {
@@ -55,46 +53,55 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        n = Integer.parseInt(st.nextToken());
-        int e = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken()); // 정점의 개수
+        e = Integer.parseInt(st.nextToken()); // 간선의 개수
+        dist = new int[n + 1];
+        visited = new boolean[n + 1];
+        graph = new ArrayList<>();
 
-        graph = new ArrayList[n + 1];
-        for (int i=0; i<=n; i++) {
-            graph[i] = new ArrayList<>();
+        for (int i = 0; i <= n; i++) {
+            graph.add(new ArrayList<>());
         }
 
-        for (int i=0; i<e; i++) {
-            st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < e; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
             int c = Integer.parseInt(st.nextToken());
-
-            graph[a].add(new Node(b, c));
-            graph[b].add(new Node(a, c));
+            graph.get(a).add(new Node(b, c));
+            graph.get(b).add(new Node(a, c));
         }
 
         st = new StringTokenizer(br.readLine(), " ");
         int v1 = Integer.parseInt(st.nextToken());
         int v2 = Integer.parseInt(st.nextToken());
 
-        int d1 = 0;
-        int d2 = 0;
         // 1 -> v1 -> v2 -> n
-        d1 += dijkstra(1, v1);
-        d1 += dijkstra(v1, v2);
-        d1 += dijkstra(v2, n);
+        int d1 = 0;
+        dijkstra(1);
+        d1 += dist[v1];
+        dijkstra(v1);
+        d1 += dist[v2];
+        dijkstra(v2);
+        d1 += dist[n];
 
         // 1 -> v2 -> v1 -> n
-        d2 += dijkstra(1, v2);
-        d2 += dijkstra(v2, v1);
-        d2 += dijkstra(v1, n);
+        int d2 = 0;
+        dijkstra(1);
+        d2 += dist[v2];
+        dijkstra(v2);
+        d2 += dist[v1];
+        dijkstra(v1);
+        d2 += dist[n];
 
-        if (d1 >= INF && d2 >= INF)
+        if (d1 >= INF && d2 >= INF) {
             bw.write(-1 + "\n");
-        else
+        } else {
             bw.write(Math.min(d1, d2) + "\n");
+        }
 
-        br.close();
+        bw.flush();
         bw.close();
+        br.close();
     }
 }
