98c5b078dade61aa1fdf600717e94228faf2d8fc
Dw-real <97527425+Dw-real@users.noreply.github.com>
2025-10-29 17:37:28 +0900

    [Silver IV] Title: 귀찮아 (SIB), Time: 272 ms, Memory: 25768 KB -BaekjoonHub

---
+++ 백준/Silver/14929. 귀찮아 （SIB）/귀찮아 （SIB）.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        long[] arr = new long[n + 1];
+        long[] sum = new long[n + 1];
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        for (int i = 1; i <= n; i++) {
+            arr[i] = Long.parseLong(st.nextToken());
+        }
+
+        for (int i = 1; i <= n; i++) {
+            sum[i] = sum[i - 1] + arr[i];
+        }
+
+        long ans = 0;
+        for (int i = 1; i <= n; i++) {
+            ans += (arr[i] * (sum[n] - sum[i]));
+        }
+
+        bw.write(ans + "\n");
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
