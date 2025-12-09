4fc661c420f96151f35f4bd6e07a81f150fcaabe
Dw-real <97527425+Dw-real@users.noreply.github.com>
2025-12-09 15:54:15 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 180 ms, Memory: 19444 KB -BaekjoonHub

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
@@ -1,94 +1,47 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
-    public static void main(String[] args) throws IOException {    
+class Main {
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        StringBuffer sb = new StringBuffer();
-
         while (true) {
             String str = br.readLine();
+            Stack<Character> stack = new Stack<>();
+
             if (str.equals("."))
                 break;
-            
-            Stack stack = new Stack();
-            boolean vps = true;
-            for (int i=0; i<str.length(); i++) {
-                char ch = str.charAt(i);
 
-                if (ch == '(' || ch == '[')
-                    stack.push(ch);
-                else if (ch == ')') {
-                    if (stack.peek() == '(')
-                        stack.pop();
-                    else {
-                        vps = false;
+            boolean valid = true;
+            for (char c : str.toCharArray()) {
+                if (c == '(' || c == '[')
+                    stack.push(c);
+                else if (c == ')') {
+                    if (stack.isEmpty() || stack.peek() != '(') {
+                        valid = false;
                         break;
                     }
-                }
-                else if (ch == ']') {
-                    if (stack.peek() == '[')
                     stack.pop();
-                    else {
-                        vps = false;
+                } else if (c == ']') {
+                    if (stack.isEmpty() || stack.peek() != '[') {
+                        valid = false;
                         break;
                     }
+                    stack.pop();
                 }
             }
-            if (stack.isEmpty() && vps) {
-                sb.append("yes" + "\n");
-            }
-            else {
-                sb.append("no" + "\n");
+
+            if (stack.isEmpty() && valid) {
+                bw.write("yes\n");
+            } else {
+                bw.write("no\n");
             }
         }
 
-        bw.write(sb.toString());
-        
-        br.close();
+        bw.flush();
         bw.close();
-    }
-}
-
-class Stack {
-    private int top;
-    private char array[];
-
-    public Stack() {
-        top = -1;
-        array = new char[100];
-    }
-
-    public boolean isFull() {
-        return top == 99;
-    }
-
-    public boolean isEmpty() {
-        return top == -1;
-    }
-
-    public void push(char item) {
-        if (isFull()) {
-            return;
-        }
-        else {
-            array[++top] = item;
-        }
-    }
-
-    public char pop() {
-        if (!isEmpty()) {
-            return array[top--];
-        }
-        return '\0';
-    }
-
-    public char peek() {
-        if (!isEmpty()) {
-            return array[top];
-        }
-        return '\0';  
+        br.close();
     }
 }
