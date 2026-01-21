dad4137e4d8a4666487bfba087a84b8aa62006c4
Dw-real <97527425+Dw-real@users.noreply.github.com>
2026-01-21 15:42:11 +0900

    [Silver III] Title: 후위 표기식2, Time: 96 ms, Memory: 14624 KB -BaekjoonHub

--- 백준/Silver/1935. 후위 표기식2/후위 표기식2.java
+++ 백준/Silver/1935. 후위 표기식2/후위 표기식2.java
@@ -1,54 +1,50 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
+class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int n = Integer.parseInt(br.readLine()); // 피연산자 개수
+        int n = Integer.parseInt(br.readLine());
+        double[] value = new double[n];
 
-        double[] a = new double[n + 1];
         String str = br.readLine();
-
-        for (int i=1; i<=n; i++) {
-            a[i] = Double.parseDouble(br.readLine());
+        for (int i = 0; i < n; i++) {
+            value[i] = Double.parseDouble(br.readLine());
         }
 
         Stack<Double> stack = new Stack<>();
 
-        for (int i=0; i<str.length(); i++) {
+        double ans = 0;
+
+        for (int i = 0; i < str.length(); i++) {
             char c = str.charAt(i);
 
-            if (c != '+' && c != '-' && c != '*' && c != '/') {
-                int num = c - 'A' + 1;
-                stack.push(a[num]);
-            }
-            else {
-                if (c == '+') {
-                    double num1 = stack.pop();
-                    double num2 = stack.pop();
-                    stack.push(num1 + num2);
-                } else if (c == '-') {
-                    double num1 = stack.pop();
-                    double num2 = stack.pop();
-                    stack.push(num2 - num1);
-                } else if (c == '*') {
-                    double num1 = stack.pop();
-                    double num2 = stack.pop();
-                    stack.push(num1 * num2);
-                } else {
-                    double num1 = stack.pop();
-                    double num2 = stack.pop();
-                    stack.push(num2 / num1);
+            if (c - '0' >= 17) { // 피연산자인 경우
+                stack.push(value[c - '0' - 17]);
+            } else {
+                double a = stack.pop();
+                double b = stack.pop();
+                switch (c) {
+                    case '+':
+                        stack.push(a + b);
+                        break;
+                    case '-':
+                        stack.push(b - a);
+                        break;
+                    case '*':
+                        stack.push(a * b);
+                        break;
+                    case '/':
+                        stack.push(b / a);
+                        break;
                 }
             }
         }
 
-        bw.write(String.format("%.2f\n", stack.peek()));
-
-        bw.flush();
-        bw.close();
+        ans = stack.pop();
+        System.out.printf("%.2f\n", ans);
         br.close();
     }
 }
