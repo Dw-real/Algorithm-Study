0179bdc4221c9fdffe1e82a93769b96bc34bc65c
Dw-real <97527425+Dw-real@users.noreply.github.com>
2026-01-22 15:03:15 +0900

    [Gold V] Title: 빗물, Time: 136 ms, Memory: 16644 KB -BaekjoonHub

--- 백준/Gold/14719. 빗물/빗물.java
+++ 백준/Gold/14719. 빗물/빗물.java
@@ -1,22 +1,37 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
+class Main {
     static int[][] map;
-    static int h,w;
+    static int h, w;
 
-    static int countRain(int x, int y) {
-        int count = 0;
-        for (int i=y+1; i<w; i++) {
-            if (map[x][i] == 0)
-                count++;
-            else if (map[x][i] == 1)
+    static int getRainwater(int x, int y) {
+        int rainWater = 0;
+
+        boolean flag = false;
+
+        while (true) {
+            y++;
+
+            if (y == w) {
                 break;
+            }
+
+            if (map[x][y] == 0) {
+                rainWater++;
+            }
+            if (map[x][y] == 1) { // 다른 블록이 있는 경우 더 이상 빗물이 고일 수 없음
+                flag = true;
+                break;
+            }
         }
-        if (map[x][w - 1] == 0 && count == w - y - 1) { // 행의 끝까지 블록을 보지 못하면 비가 고이지 않음
-            count = 0;
+
+        // 끝까지 이동했을 때 블록을 보지 못한 경우 빗물이 고일 수 없음
+        if (!flag) {
+            rainWater = 0;
         }
-        return count;
+
+        return rainWater;
     }
 
     public static void main(String[] args) throws IOException {
@@ -30,24 +45,24 @@ public class Main {
         map = new int[h][w];
 
         st = new StringTokenizer(br.readLine(), " ");
-
-        for (int i=0; i<w; i++) {
-            int height = Integer.parseInt(st.nextToken()); // 블록이 쌓인 높이
-            for (int j=h-height; j<h; j++) {
-                map[j][i] = 1;
+        for (int i = 0; i < w; i++) {
+            int height = Integer.parseInt(st.nextToken());
+            for (int j = h - 1; j >= h - height; j--) {
+                map[j][i] = 1; // 블록
             }
         }
 
-        int sum = 0;
-        for (int i=0; i<h; i++) {
-            for (int j=0; j<w; j++) {
+        int ans = 0;
+
+        for (int i = 0; i < h; i++) {
+            for (int j = 0; j < w; j++) {
                 if (map[i][j] == 1) {
-                    sum += countRain(i, j);
+                    ans += getRainwater(i, j);
                 }
             }
         }
 
-        bw.write(sum + "\n");
+        bw.write(ans + "\n");
 
         bw.flush();
         bw.close();
