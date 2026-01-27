494a98085188c19270fba70a9ef7362f3240a4e7
Dw-real <97527425+Dw-real@users.noreply.github.com>
2026-01-27 20:00:54 +0900

    [Gold IV] Title: 미세먼지 안녕!, Time: 420 ms, Memory: 71720 KB -BaekjoonHub

--- 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.java
+++ 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.java
@@ -1,84 +1,92 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
-    static int[] dx = {-1, 0, 1 ,0};
-    static int[] dy = {0, 1, 0, -1};
+class Main {
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
     static int[][] room;
-    static int r, c;
-    static int airPos; // 공기청정기 위치
+    static int r, c, t;
+    static int up, down; // 공기 청정기의 위쪽, 아래쪽
 
-    static int[][] spreadDust(int[][] room) { // 미세먼지 확산
-        int[][] after = new int[r + 1][c + 1];
-
-        for (int i=1; i<=r; i++) {
-            for (int j=1; j<=c; j++) {
-                after[i][j] = room[i][j];
-            }
+    static int[][] operateAirPurifier() {
+        room = spreadDust();
+        int[][] after = new int[r][c];
+        for (int i = 0; i < r; i++) {
+            after[i] = room[i].clone();
         }
 
-        for (int i=1; i<=r; i++) {
-            for (int j=1; j<=c; j++){
-                if (room[i][j] == -1 || room[i][j] == 0) continue; // 공기 청정기 자리거나 미세먼지가 없으면 지나감
-                else {
-                    int spread = room[i][j] / 5;
-                    for (int k=0; k<4; k++) {
+        // 공기 청정기 위쪽 -> 반시계 방향
+        for (int i = 2; i < c; i++) {
+            after[up][i] = room[up][i - 1];
+        }
+
+        for (int i = up - 1; i >= 0; i--) {
+            after[i][c - 1] = room[i + 1][c - 1];
+        }
+
+        for (int i = c - 2; i >= 0; i--) {
+            after[0][i] = room[0][i + 1];
+        }
+
+        for (int i = 1; i < up; i++) {
+            if (room[i - 1][0] == -1) continue;
+            after[i][0] = room[i - 1][0];
+        }
+
+        after[up][1] = 0;
+        after[up][0] = -1;
+
+        // 공기 청정기 아래쪽 -> 시계 방향
+        for (int i = 2; i < c; i++) {
+            after[down][i] = room[down][i - 1];
+        }
+
+        for (int i = down + 1; i < r; i++) {
+            after[i][c - 1] = room[i - 1][c - 1];
+        }
+
+        for (int i = c - 2; i >= 0; i--) {
+            after[r - 1][i] = room[r - 1][i + 1];
+        }
+
+        for (int i = r - 2; i > down; i--) {
+            if (room[i + 1][0] == -1) continue;
+            after[i][0] = room[i + 1][0];
+        }
+
+        after[down][1] = 0;
+        after[down][0] = -1;
+
+        return after;
+    }
+
+    static int[][] spreadDust() {
+        int[][] after = new int[r][c];
+
+        for (int i = 0; i < r; i++) {
+            after[i] = room[i].clone();
+        }
+
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                if (room[i][j] > 0) {
+                    int dust = room[i][j];
+                    int d = dust / 5; // 인접한 칸에 확산되는 미세먼지의 양
+                    for (int k = 0; k < 4; k++) {
                         int nx = i + dx[k];
                         int ny = j + dy[k];
 
-                        if (nx < 1 || nx > r || ny < 1 || ny > c) continue;
-                        if (room[nx][ny] == -1) continue; // 공기청정기 위치는 확산되지 않음
+                        // 인접한 칸이 없거나 공기청정기가 있는 경우는 확산되지 않음
+                        if (nx < 0 || nx >= r || ny < 0 || ny >= c) continue;
+                        if (room[nx][ny] == -1) continue;
 
-                        after[nx][ny] += spread;
-                        after[i][j] -= spread;
+                        after[nx][ny] += d;
+                        after[i][j] -= d;
                     }
                 }
             }
         }
-        return after;
-    }
 
-    static int[][] operate(int[][] room) {
-        int[][] after = new int[r + 1][c + 1];
-
-        for (int i=1; i<=r; i++) {
-            for (int j=1; j<=c; j++) {
-                after[i][j] = room[i][j];
-            }
-        }
-        // 공기 청정기의 윗부분 (반시계 방향)
-        for (int x=airPos - 1; x> 1; x--) {
-            if (room[x][1] == -1) continue;
-            after[x][1] = room[x - 1][1];
-        }
-        for (int y=1; y<c; y++) {
-            after[1][y] = room[1][y + 1];
-        }
-        for (int x=1; x<airPos-1; x++) {
-            after[x][c] = room[x + 1][c];
-        }
-        for (int y=c; y > 1; y--) {
-            after[airPos - 1][y] = room[airPos - 1][y - 1];
-        }
-        after[airPos - 1][2] = 0; // 공기청정기에서 나온 공기
-        after[airPos - 1][1] = -1; // 공기청정기의 위치 유지
-
-        // 공기 청정기의 아랫 부분 (시계 방향)
-        for (int x=airPos; x < r; x++) {
-            if (room[x][1] == -1) continue;
-            after[x][1] = room[x + 1][1];
-        }
-        for (int y=1; y<c; y++) {
-            after[r][y] = room[r][y + 1];
-        }
-        for (int x=r; x>airPos; x--) {
-            after[x][c] = room[x - 1][c];
-        }
-        for (int y=c; y>1; y--) {
-            after[airPos][y] = room[airPos][y - 1];
-        }
-        after[airPos][2] = 0; // 공기청정기에서 나온 공기
-        after[airPos][1] = -1; // 공기청정기의 위치 유지
         return after;
     }
 
@@ -89,38 +97,38 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         r = Integer.parseInt(st.nextToken());
         c = Integer.parseInt(st.nextToken());
-        int t = Integer.parseInt(st.nextToken());
+        t = Integer.parseInt(st.nextToken());
 
-        room = new int[r + 1][c + 1];
+        room = new int[r][c];
 
-        for (int i=1; i<=r; i++) {
+        boolean discover = false; // 공기청정기의 위쪽 발견 여부
+
+        for (int i = 0; i < r; i++) {
             st = new StringTokenizer(br.readLine(), " ");
-            for (int j=1; j<=c; j++){
+            for (int j = 0; j < c; j++) {
                 room[i][j] = Integer.parseInt(st.nextToken());
-
-                if (room[i][j] == -1)
-                    airPos = i; // 공기청정기의 밑부분
+                if (room[i][j] == -1 && !discover) { // 공기청정기가 있는 위치
+                    up = i;
+                    down = i + 1;
+                    discover = true;
+                }
             }
         }
 
+        int totalDust = 0; // 방에 남아있는 미세먼지의 양
 
-        for (int time = 0; time<t; time++) {
-            int[][] after = spreadDust(room); // 미세먼지 확산
-            after = operate(after); // 공기청정기 작동
-            room = after;
+        for (int i = 0; i < t; i++) {
+            room = operateAirPurifier();
         }
-
-        int sum = 0;
-        for (int i=1; i<=r; i++) {
-            for (int j=1; j<=c; j++){
-                if (room[i][j] == -1 || room[i][j] == 0) continue;
-                sum += room[i][j];
-                //System.out.print(room[i][j] + " ");
+        
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                if (room[i][j] == -1) continue; // 공기청정기가 있는 위치
+                totalDust += room[i][j];
             }
-            //System.out.println();
         }
 
-        bw.write(sum + "\n");
+        bw.write(totalDust + "\n");
 
         bw.flush();
         bw.close();
